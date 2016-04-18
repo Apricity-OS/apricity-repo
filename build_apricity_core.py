f786685ce3d7cbc6c9ce61e8dabe26592ec86d7d
Alex Gajewski <agajews@gmail.com>
2016-04-18 17:21:27 -0500

    Added apricity packages

--- build_apricity_core.py
+++ build_apricity_core.py
@@ -2,8 +2,9 @@ from subprocess import call
 from os import mkdir
 from shutil import copy, rmtree
 from glob import glob
-from lib import cd, make_tarfile
+from lib import cd
 from packages import YaourtPackage, ApricityPackage
+from argparse import ArgumentParser
 
 
 def get_packages():
@@ -12,23 +13,19 @@ def get_packages():
                        'firefox-extension-shumway', 'google-chrome',
                        'google-talkplugin', 'gst-plugin-libde265',
                        'kpmcore-git', 'libde265', 'libtimezonemap',
-                       'nightingale-git', 'otter-browser', 'pacaur',
+                       'otter-browser', 'pacaur',
                        'package-query-git', 'palemoon-bin', 'atom-editor',
-                       'pamac-aur', 'pam_encfs', 'pipelight', 'plymouth',
+                       'pamac-aur', 'pam_encfs', 'plymouth',
                        'polkit-gnome-gtk2', 'pyparted-git',
                        'python2-powerline-git', 'python-beautifulsoup4',
                        'python-parted', 'sbackup', 'telegram-desktop-bin',
                        'ttf-ms-fonts', 'v86d', 'vte3-notification',
                        'vte3-notification-common', 'wine', 'wine-silverlight',
                        'yaourt-git']
-    yaourt_packages_truncated = ['pyparted-git']
-    # yaourt_packages = yaourt_packages_truncated
     apricity_packages = ['apricityassets', 'apricity-vim', 'ice-ssb',
                          'calamares', 'apricity-wallpapers'
                          'apricity-themes-gnome', 'apricity-themes-cinnamon',
                          'apricity-icons', 'apricity-chrome-profile']
-    apricity_packages_truncated = ['ice-ssb']
-    # apricity_packages = apricity_packages_truncated
     packages = []
     for package_name in yaourt_packages:
         packages.append(YaourtPackage(package_name))
@@ -40,30 +37,55 @@ def get_packages():
 def clean():
     try:
         rmtree('build')
-        rmtree('repo')
+        rmtree('core')
     except Exception as e:
         print(e)
 
 
-def build_core(packages):
-    mkdir('build')
-    mkdir('repo')
+def sync_core():
+    call(['rsync', '-aP', 'core', 'apricity@apricityos.com/public_html/apricity-core'])
+
+
+def build_core(packages, install_makedeps=True, verbose=True):
+    build_dir = 'build'
+    repo_dir = 'core'
+    mkdir(build_dir)
+    mkdir(repo_dir)
     for package in packages:
-        package.build('build')
-        pkgs = glob('build/' + package.pkg + '/' '*.pkg.tar.xz')
+        if install_makedeps:
+            package.install_makedeps(verbose=verbose)
+        package.build(build_dir, verbose=verbose)
+        pkgs = glob(build_dir + '/' + package.name + '/' '*.pkg.tar.xz')
         if len(pkgs) > 0:
             for file in pkgs:
-                copy(file, 'repo')
-        else raise Exception('Makepkg failed')
-    with cd('repo'):
+                copy(file, repo_dir)
+        else:
+            raise Exception('Makepkg failed')
+    with cd(repo_dir):
         call('repo-add apricity-core.db.tar.gz *.pkg.tar.xz', shell=True)
 
 
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('-m', '--install_makedeps',
+                        help='install make dependencies',
+                        action='store_true')
+    parser.add_argument('-v', '--verbose',
+                        help='increase verbosity',
+                        action='store_true')
+    args = parser.parse_args()
+    return args
+
+
 def main():
+    args = get_args()
+    print('Verbosity: ' + str(args.verbose))
     with cd('~/Apricity-OS/apricity-repo'):
         clean()
         packages = get_packages()
-        build_core(packages)
+        build_core(packages, install_makedeps=args.install_makedeps,
+                   verbose=args.verbose)
+        sync_core()
 
 
 if __name__ == '__main__':
--- packages.py
+++ packages.py
@@ -1,26 +1,44 @@
 from lib import cd, make_tarfile
 from subprocess import call
-from glob import glob
+from os import devnull
 
 
 class YaourtPackage():
-    def __init__(self, pkg):
-        self.pkg = pkg
+    def __init__(self, name):
+        self.name = name
 
-    def build(self, build):
+    def build(self, build, verbose=True):
         with cd(build):
-            call('yaourt -G ' + self.pkg, shell=True)
-            with cd(self.pkg):
-                call('makepkg', shell=True)
+            if verbose:
+                call(['yaourt', '-G', self.name])
+            else:
+                call(['yaourt', '-G', self.name], stdout=open(devnull, 'w'))
+            with cd(self.name):
+                if verbose:
+                    call('makepkg')
+                else:
+                    call('makepkg', stdout=open(devnull, 'w'))
+
+    def install_makedeps(self, verbose=True):
+        if verbose:
+            call(['yaourt', '-S', '--noconfirm', self.name])
+        else:
+            call(['yaourt', '-S', '--noconfirm', self.name], stdout=open(devnull, 'w'))
 
 
 class ApricityPackage():
-    def __init__(self, pkg):
-        self.pkg = pkg
+    def __init__(self, name):
+        self.name = name
 
-    def build(self, build):
+    def build(self, build, verbose=True):
         with cd(build):
-            call('git clone ' + 'https://github.com/Apricity-OS/' + self.pkg + '.git', shell=True)
-            with cd(self.pkg):
-                make_tarfile(self.pkg + '.tar.gz', 'src/' + self.pkg)
-                call('makepkg')
+            call(['git', 'clone', 'https://github.com/Apricity-OS/' + self.name + '.git'])
+            with cd(self.name):
+                make_tarfile(self.name + '.tar.gz', 'src/' + self.name)
+                if verbose:
+                    call('makepkg')
+                else:
+                    call('makepkg', stdout=open(devnull, 'w'))
+
+    def install_makedeps(self, verbose=True):
+        pass
