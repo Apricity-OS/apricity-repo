d88ab0e0adeaf11cd9633307706700d73da7c8d3
Alex Gajewski <agajews@gmail.com>
2016-04-20 21:31:01 -0500

    Removed vte3 errors, fixed commas, added failure checks

--- build_apricity_core.py
+++ build_apricity_core.py
@@ -1,4 +1,4 @@
-from subprocess import call
+from subprocess import call, CalledProcessError
 from os import mkdir
 from shutil import copy, rmtree
 from glob import glob
@@ -19,11 +19,11 @@ def get_packages():
                        'polkit-gnome-gtk2', 'pyparted-git',
                        'python2-powerline-git', 'python-beautifulsoup4',
                        'sbackup', 'telegram-desktop-bin',
-                       'ttf-ms-fonts', 'v86d', 'vte3-notification',
-                       'vte3-notification-common', 'wine', 'wine-silverlight',
+                       'ttf-ms-fonts', 'v86d',  # 'vte3-notification',
+                       'wine', 'wine-silverlight',
                        'yaourt-git']
     apricity_packages = ['apricityassets', 'apricity-vim', 'ice-ssb',
-                         'calamares', 'apricity-wallpapers'
+                         'calamares', 'apricity-wallpapers',
                          'apricity-themes-gnome', 'apricity-themes-cinnamon',
                          'apricity-icons', 'apricity-chrome-profile']
     packages = []
@@ -37,6 +37,9 @@ def get_packages():
 def clean():
     try:
         rmtree('build')
+    except Exception as e:
+        print(e)
+    try:
         rmtree('core')
     except Exception as e:
         print(e)
@@ -46,23 +49,37 @@ def sync_core():
     call(['rsync', '-aP', 'core', 'apricity@apricityos.com/public_html/apricity-core'])
 
 
-def build_core(packages, install_makedeps=True, verbose=True):
+def build_core(packages, install_makedeps=True, verbose=True, max_attempts=5):
     build_dir = 'build'
     repo_dir = 'core'
     mkdir(build_dir)
     mkdir(repo_dir)
+    failed = []
     for package in packages:
-        if install_makedeps:
-            package.install_makedeps(verbose=verbose)
-        package.build(build_dir, verbose=verbose)
-        pkgs = glob(build_dir + '/' + package.name + '/' '*.pkg.tar.xz')
-        if len(pkgs) > 0:
-            for file in pkgs:
-                copy(file, repo_dir)
-        else:
-            raise Exception('Makepkg failed')
+        attempts = 0
+        while attempts < max_attempts:
+            try:
+                if install_makedeps:
+                    package.install_makedeps(verbose=verbose)
+                package.build(build_dir, verbose=verbose)
+                pkgs = glob(build_dir + '/' + package.name + '/*.pkg.tar.xz')
+                if len(pkgs) > 0:
+                    for file in pkgs:
+                        copy(file, repo_dir)
+                else:
+                    raise Exception('Makepkg failed')
+                break
+            except Exception as e:
+                print('Unexpected Error:' + str(e))
+                attempts += 1
+                if attempts < max_attempts:
+                    print('Trying again...')
+                    rmtree('build/' + package.name, ignore_errors=True)
+                else:
+                    failed.append(package.name)
     with cd(repo_dir):
         call('repo-add apricity-core.db.tar.gz *.pkg.tar.xz', shell=True)
+    return failed
 
 
 def get_args():
@@ -83,9 +100,14 @@ def main():
     with cd('~/Apricity-OS/apricity-repo'):
         clean()
         packages = get_packages()
-        build_core(packages, install_makedeps=args.install_makedeps,
-                   verbose=args.verbose)
+        failed_packages = build_core(packages,
+                                     install_makedeps=args.install_makedeps,
+                                     verbose=args.verbose)
         sync_core()
+        if len(failed_packages) > 0:
+            print('Failed packages:')
+            for package_name in failed_packages:
+                print(package_name)
 
 
 if __name__ == '__main__':
