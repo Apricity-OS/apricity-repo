198e39a09b172b44eb1433a2418d543c4310e3ae
Alex Gajewski <agajews@gmail.com>
2016-08-01 22:44:37 -0500

    Switched to static domain

--- apricity_buildpush.sh
+++ apricity_buildpush.sh
@@ -19,15 +19,15 @@ git clone https://github.com/Apricity-OS/${package_name}
 cd ${package_name}
 makepkg -s --sign --clean --noconfirm 2>&1 | tee ${package_name}.log
 
-wget 192.241.147.116/${repo_endpoint}/${repo_name}.db
-wget 192.241.147.116/${repo_endpoint}/${repo_name}.db.tar.gz
-wget 192.241.147.116/${repo_endpoint}/${repo_name}.files
-wget 192.241.147.116/${repo_endpoint}/${repo_name}.files.tar.gz
+wget static.apricityos.com/${repo_endpoint}/${repo_name}.db
+wget static.apricityos.com/${repo_endpoint}/${repo_name}.db.tar.gz
+wget static.apricityos.com/${repo_endpoint}/${repo_name}.files
+wget static.apricityos.com/${repo_endpoint}/${repo_name}.files.tar.gz
 
 repo-add --sign ${repo_name}.db.tar.gz ${package_name}*.pkg.tar.xz
 
-scp ${repo_name}.db* ${repo_name}.files* ${package_name}*.pkg.tar.xz server@192.241.147.116:/mnt/static/public_html/${repo_endpoint}/
-scp ${package_name}.log server@192.241.147.116:/mnt/static/public_html/${repo_endpoint}/
+scp ${repo_name}.db* ${repo_name}.files* ${package_name}*.pkg.tar.xz server@static.apricityos.com:/mnt/static/public_html/${repo_endpoint}/
+scp ${package_name}.log server@static.apricityos.com:/mnt/static/public_html/${repo_endpoint}/
 
 cd ../..
 rm -rf build
